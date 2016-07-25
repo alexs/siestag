36790a6fd1dd079c33b22b7bec4661aad039628c
Alex Silva <alexs1010@gmail.com>
2016-07-25 11:58:59 -0500

    minor changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
     user_name:            Rails.application.secrets.email_user,
     password:             Rails.application.secrets.email_password,
     authentication:       'plain',
-    enable_starttls_auto: true  }
+    enable_starttls_auto: false  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
